fbde9ba7cf1eec6a85d254c144e55a4936e8c3eb
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-10 16:35:18 +0100

    Used constant interpolated string

--- Csharp10.NewFeatures/Program.cs
+++ Csharp10.NewFeatures/Program.cs
@@ -1,2 +1,6 @@
-﻿// used "file scoped namespace"
-WriteLine(typeof(MySpecialNamespaceDefinedForTheWholeFile.Person).FullName);
+﻿const string name = "John Doe";
+const string age = "20";
+// now we can have constant interpolated strings
+const string helloMessage = $"Hello {name}, your age is {age}";
+
+WriteLine(helloMessage);
