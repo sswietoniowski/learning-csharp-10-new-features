d3527b472b027f434d08b388d13dd5d32ff9cc85
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-10 16:20:51 +0100

    Added global using

---
+++ Csharp10.NewFeatures/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿// now we can use global using for the most common namespaces
+global using System;
+global using static System.Console;
--- Csharp10.NewFeatures/Program.cs
+++ Csharp10.NewFeatures/Program.cs
@@ -1,1 +1,2 @@
-﻿
+﻿// thanks to the global using we can write something like that :-)
+WriteLine("Hello thanks to the global using");
