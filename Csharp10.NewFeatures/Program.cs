749be9cbf26ac5838e84b2d0b00af7d1e4b0560d
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-10 16:41:02 +0100

    Used improved (simplified) syntax for lambdas

--- Csharp10.NewFeatures/Program.cs
+++ Csharp10.NewFeatures/Program.cs
@@ -1,6 +1,14 @@
-﻿const string name = "John Doe";
-const string age = "20";
-// now we can have constant interpolated strings
-const string helloMessage = $"Hello {name}, your age is {age}";
+﻿// there are many improvements conserning lambdas
 
-WriteLine(helloMessage);
+// this is how we could do that in the old days
+
+Func<string> helloWorld = () => "Hello World!";
+
+// now we could use type inference
+
+var newHelloWorld = () => "New Hello World!";
+
+WriteLine(helloWorld());
+WriteLine(newHelloWorld());
+
+var text = string? () => null; // in the past you could not do that (after all null can be anything)
